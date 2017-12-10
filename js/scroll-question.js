65565dd978b683bde27195cb2f1701319a95c518
Michel van Heest <michelvanheest@icloud.com>
2017-12-10 21:17:27 +0100

    probably fixed it

--- js/scroll-question.js
+++ js/scroll-question.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},1000);
+                $(this).animate({'opacity':'1'},200);
             }
         });
     });
