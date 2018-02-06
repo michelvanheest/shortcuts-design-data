37c3e142115c9324f0d6085a67f4e7836148081b
Michel van Heest <michelvanheest@icloud.com>
2018-02-06 17:08:33 +0100

    Adding Windows for Adobe XD + trying to build it global

---
+++ js/switch.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
