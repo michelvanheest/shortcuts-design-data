995491ac05c47901e70a1f3a00a244b3281cc8d9
Michel van Heest <michelvanheest@icloud.com>
2018-08-08 20:44:28 +0200

    Changed dev-color + added productivity

--- js/nav-trans.js
+++ js/nav-trans.js
@@ -14,6 +14,14 @@ $('#tools-development-click-trans').on('click', function(e){
     }, 1000);
 });
 
+$('#tools-productivity-click-trans').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'tools-productivity-click-trans' ? $('#tools-productivity-click') : $('#tools-productivity-click-trans');
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 1000);
+});
+
 $('#tools-miscellaneous-click-trans').on('click', function(e){
     e.preventDefault();
     var target= $(this).get(0).id == 'tools-miscellaneous-click-trans' ? $('#tools-miscellaneous-click') : $('#tools-miscellaneous-click-trans');
