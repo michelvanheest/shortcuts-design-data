6befbe2ee3b7adfae007328dc882c3fe747f6c84
Michel van Heest <michelvanheest@icloud.com>
2019-02-18 14:28:45 +0100

    Fixed search function for Google Sheets

--- js/search.js
+++ js/search.js
@@ -1,7 +1,18 @@
+// Search function GitHub Submodules
 $(document).ready(function(){
     $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+    });
+});
+
+// Search function Google Sheets
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".google-visualization-table-table tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
     });
