04dfb377f0bb651016be9898d4bdd36c47fb7ad5
Michel van Heest <michelvanheest@icloud.com>
2019-03-02 19:48:55 +0100

    reverted ‘tbody’ to ‘tr’ after making HTML simpler

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
     $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#myTable tbody").filter(function() {
+    $("#myTable tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
     });
