43f06f95ac62b65cc72667fb3041c230a2d1959b
Michel van Heest <michelvanheest@icloud.com>
2021-02-27 16:34:42 +0100

    Added search button

--- js/animate.js
+++ js/animate.js
@@ -10,3 +10,25 @@ $(function() {
         });
     });
 });
+
+// Fade in scroll-button.njk
+$(function() {
+    $(window).scroll( function(){
+        $('#scroll-to-search').each( function(i){
+            var bottom_of_object = $(this).position().top + 500;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},600);
+            }
+        });
+    });
+});
+
+// Scroll to searchbar
+$('#scroll-to-search').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'scroll-to-search' ? $('#myInput') : $('#scroll-to-search');
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 600);
+});
