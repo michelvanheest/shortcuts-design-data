7e29f7c4196b3792df6c14941801429129247e26
Michel van Heest <michelvanheest@icloud.com>
2021-04-20 18:58:41 +0200

    Moved data + fixed main paths

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/fonts");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
-    eleventyConfig.addPassthroughCopy("data-live");
+    eleventyConfig.addPassthroughCopy("data");
     eleventyConfig.addPassthroughCopy("_redirects");
     eleventyConfig.addPassthroughCopy("shortcuts-design-social.jpg");
     eleventyConfig.addPassthroughCopy("shortcuts-design-favicon.ico");
