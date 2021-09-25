a71f8f5a5127b018ca8c772e9090544e4cb2a2b9
Michel van Heest <michelvanheest@icloud.com>
2021-09-25 16:53:30 +0200

    Load data and add class to all th's

--- js/search.js
+++ js/search.js
@@ -2,10 +2,10 @@
 $(document).ready(function(){
     $("#myInput").on("keyup", function() {
         var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
+        $(".data-table #myTable tr").filter(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
-        if($("#myTable tr").children(":visible").length == 0) {
+        if($(".data-table #myTable tr").children(":visible").length == 0) {
             $(".error").show();
         } else {
             $(".error").hide();
