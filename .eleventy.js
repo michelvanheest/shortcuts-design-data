42043bd3011bc5336437f6ce1dc5d7dc4b9442db
Michel van Heest <michelvanheest@icloud.com>
2023-03-24 20:02:35 +0100

    Added custom sorting

--- .eleventy.js
+++ .eleventy.js
@@ -20,8 +20,32 @@ module.exports = function(eleventyConfig) {
             else return 0;
         });
     });
-}
 
+    // eleventyConfig.addCollection("tools", function (collection) {
+    //     const insert = (arr, index, newItem) => [
+    //         // part of the array before the specified index
+    //         ...arr.slice(0, index),
+    //         // inserted item
+    //         newItem,
+    //         // part of the array after the specified index
+    //         ...arr.slice(index)
+    //     ]
+    
+    //     const cards = collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
+    //         let nameA = a.data.tool.toUpperCase();
+    //         let nameB = b.data.tool.toUpperCase();
+    //         if (nameA < nameB) return -1;
+    //         else if (nameA > nameB) return 1;
+    //         else return 0;
+    //     });
+    
+    //     const ejectedCard = cards.filter(c => c.data.tool.toLowerCase() === 'veedio');
+    //     const restCards = cards.filter(c=>c.data.tool.toLowerCase() !== 'veedio')
+    //     const newSortedCards = insert(restCards, 3, ejectedCard[0])
+    
+    //     return newSortedCards
+    // });
+}
 
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
@@ -38,5 +62,5 @@ const imageminPngquant = require('imagemin-pngquant');
         ]
     });
 
-    console.log(files);
+    // console.log(files);
 })();
