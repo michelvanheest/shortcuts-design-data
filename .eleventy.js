b4435bcaae1a8d214b67bb4649110bc7111a4513
Michel van Heest <michelvanheest@icloud.com>
2023-04-22 13:21:51 +0200

    feature: added custom sorting for veed

--- .eleventy.js
+++ .eleventy.js
@@ -11,40 +11,40 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("shortcuts-design-favicon.ico");
     eleventyConfig.addPassthroughCopy("shortcuts-design-favicon.png");
     eleventyConfig.addPassthroughCopy("sitemap.xml");
-    eleventyConfig.addCollection("tools", function(collection) {
-        return collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
-            let nameA = a.data.tool.toUpperCase();
-            let nameB = b.data.tool.toUpperCase();
-            if (nameA < nameB) return -1;
-            else if (nameA > nameB) return 1;
-            else return 0;
-        });
-    });
-
-    // eleventyConfig.addCollection("tools", function (collection) {
-    //     const insert = (arr, index, newItem) => [
-    //         // part of the array before the specified index
-    //         ...arr.slice(0, index),
-    //         // inserted item
-    //         newItem,
-    //         // part of the array after the specified index
-    //         ...arr.slice(index)
-    //     ]
-    
-    //     const cards = collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
+    // eleventyConfig.addCollection("tools", function(collection) {
+    //     return collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
     //         let nameA = a.data.tool.toUpperCase();
     //         let nameB = b.data.tool.toUpperCase();
     //         if (nameA < nameB) return -1;
     //         else if (nameA > nameB) return 1;
     //         else return 0;
     //     });
-    
-    //     const ejectedCard = cards.filter(c => c.data.tool.toLowerCase() === 'veedio');
-    //     const restCards = cards.filter(c=>c.data.tool.toLowerCase() !== 'veedio')
-    //     const newSortedCards = insert(restCards, 3, ejectedCard[0])
-    
-    //     return newSortedCards
     // });
+
+    eleventyConfig.addCollection("tools", function (collection) {
+        const insert = (arr, index, newItem) => [
+            // part of the array before the specified index
+            ...arr.slice(0, index),
+            // inserted item
+            newItem,
+            // part of the array after the specified index
+            ...arr.slice(index)
+        ]
+    
+        const cards = collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
+            let nameA = a.data.tool.toUpperCase();
+            let nameB = b.data.tool.toUpperCase();
+            if (nameA < nameB) return -1;
+            else if (nameA > nameB) return 1;
+            else return 0;
+        });
+    
+        const ejectedCard = cards.filter(c => c.data.tool.toLowerCase() === 'veed');
+        const restCards = cards.filter(c=>c.data.tool.toLowerCase() !== 'veed')
+        const newSortedCards = insert(restCards, 3, ejectedCard[0])
+    
+        return newSortedCards
+    });
 }
 
 const imagemin = require('imagemin');
