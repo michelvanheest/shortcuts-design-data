f0531986149d3bc5ef8d405bac43d869f0336fe3
Michel van Heest <michelvanheest@icloud.com>
2023-08-10 09:38:45 +0200

    chore: ended VEED featuring

--- .eleventy.js
+++ .eleventy.js
@@ -11,40 +11,43 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("shortcuts-design-favicon.ico");
     eleventyConfig.addPassthroughCopy("shortcuts-design-favicon.png");
     eleventyConfig.addPassthroughCopy("sitemap.xml");
-    // eleventyConfig.addCollection("tools", function(collection) {
-    //     return collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
-    //         let nameA = a.data.tool.toUpperCase();
-    //         let nameB = b.data.tool.toUpperCase();
-    //         if (nameA < nameB) return -1;
-    //         else if (nameA > nameB) return 1;
-    //         else return 0;
-    //     });
-    // });
 
-    eleventyConfig.addCollection("tools", function (collection) {
-        const insert = (arr, index, newItem) => [
-            // part of the array before the specified index
-            ...arr.slice(0, index),
-            // inserted item
-            newItem,
-            // part of the array after the specified index
-            ...arr.slice(index)
-        ]
-    
-        const cards = collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
+    // Default sorting of all tools on home
+    eleventyConfig.addCollection("tools", function(collection) {
+        return collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
             let nameA = a.data.tool.toUpperCase();
             let nameB = b.data.tool.toUpperCase();
             if (nameA < nameB) return -1;
             else if (nameA > nameB) return 1;
             else return 0;
         });
-    
-        const ejectedCard = cards.filter(c => c.data.tool.toLowerCase() === 'veed');
-        const restCards = cards.filter(c=>c.data.tool.toLowerCase() !== 'veed')
-        const newSortedCards = insert(restCards, 3, ejectedCard[0])
-    
-        return newSortedCards
     });
+    
+    // Custom sorting of all tools on home where we use the default sorting and place a specific card on a specific place
+    // eleventyConfig.addCollection("tools", function (collection) {
+    //     const insert = (arr, index, newItem) => [
+    //         // part of the array before the specified index
+    //         ...arr.slice(0, index),
+    //         // inserted item
+    //         newItem,
+    //         // part of the array after the specified index
+    //         ...arr.slice(index)
+    //     ]
+    
+    //     const cards = collection.getFilteredByGlob("tools/*.md").sort(function(a, b) {
+    //         let nameA = a.data.tool.toUpperCase();
+    //         let nameB = b.data.tool.toUpperCase();
+    //         if (nameA < nameB) return -1;
+    //         else if (nameA > nameB) return 1;
+    //         else return 0;
+    //     });
+    
+    //     const ejectedCard = cards.filter(c => c.data.tool.toLowerCase() === 'veed');
+    //     const restCards = cards.filter(c=>c.data.tool.toLowerCase() !== 'veed')
+    //     const newSortedCards = insert(restCards, 3, ejectedCard[0])
+    
+    //     return newSortedCards
+    // });
 }
 
 const imagemin = require('imagemin');
